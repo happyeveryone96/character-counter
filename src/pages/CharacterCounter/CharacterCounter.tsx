1cb88dfa2e4d67362e7b68065e48bc5370d6f416
happyeveryone96 <happyeveryone@naver.com>
2022-08-18 17:24:19 +0900

    Add: 프로젝트 초기 설정

---
+++ src/pages/CharacterCounter/CharacterCounter.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function CharacterCounter() {
+  return <div>CharacterCounter</div>;
+}
+
+export default CharacterCounter;
---
+++ src/pages/Router.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import CharacterCounter from './CharacterCounter/CharacterCounter';
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<CharacterCounter />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
